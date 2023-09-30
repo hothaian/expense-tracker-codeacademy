8c9eefee181db6dfdcaca1092e780961763ad519
An Ho <hothaian1999@gmail.com>
2023-09-29 17:10:18 -0700

    first

---
+++ src/features/budgets/budgetsSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const CATEGORIES = ['housing', 'food', 'transportation', 'utilities', 'clothing', 'healthcare', 'personal', 'education', 'entertainment'];
+
+const initialState = CATEGORIES.map(category => ({ category: category, amount: 0 }))
+
+const budgetsSlice = createSlice({
+  name: 'budgets',
+  initialState: initialState,
+  reducers: {
+    editBudget: (state, action) => {
+      const budgetToUpdate = state.find(budget => budget.category === action.payload.category);
+      budgetToUpdate.amount = action.payload.amount;
+    }
+  }
+});
+
+export const selectBudgets = (state) => state.budgets;
+export const { editBudget } = budgetsSlice.actions;
+export default budgetsSlice.reducer;
+
